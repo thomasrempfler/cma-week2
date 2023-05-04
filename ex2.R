90debb0c7d67ac4b654e5ae248468420c57dc85e
Thomas Rempfler <rempftho@students.zhaw.ch>
2023-05-04 06:45:49 +0200

    tasks 1 - 3 completed

--- ex2.R
+++ ex2.R
@@ -17,35 +17,57 @@ wildschwein_BE <- read_delim("wildschwein_BE_2056.csv", ",")
 
 wildschwein_BE <- st_as_sf(wildschwein_BE, coords = c("E", "N"), crs = 2056, remove = FALSE)
   
+
 # task 2 ----
 
 # time intervals?
 ws <- wildschwein_BE |> 
-  group_by(TierID) |> # ...group it by TierID
-  mutate(timelag = as.numeric(difftime(lead(DatetimeUTC), DatetimeUTC), units = "secs"))
+  group_by(TierID) |> 
+  mutate(timelag = as.integer(difftime(lead(DatetimeUTC), DatetimeUTC), units = "secs"))
 ws
 
-# how many ids?
+# how many individuals were tracked? -> 3 individuals (Sabi, Ruth, Rosa)
 ggplot(ws, aes(DatetimeUTC, TierName)) +
   geom_point()
 
+ws |>
+  distinct(TierID)
+
+# for how long were they tracked?
+ws |> 
+  group_by(TierName) |> 
+  mutate(date_min = min(DatetimeUTC), date_max = max(DatetimeUTC)) |> 
+  distinct(date_min, date_max)
+
+# are there gaps?
 ggplot(ws, aes(timelag/60)) +
   geom_histogram(binwidth = 1) +
   lims(x = c(0, 5000/60)) +
   scale_y_log10()
 
-# explore schedules
+# were they tracked concurrently or sequentially?
 ggplot(ws, aes(DatetimeUTC, timelag, colour = TierName)) +
   geom_point()
 
-# task 3
-ws <- ws |> # Take wildschwein...
-  group_by(TierID) |> # ...group it by TierID
+# what is the temporal sampling interval?
+ws |> 
+  group_by(TierName, timelag) |> 
+  distinct()
+  
+ggplot(ws, aes(DatetimeUTC, timelag/60, colour = TierName)) +
+  geom_point()
+
+
+# task 3 ----
+
+# euclidean distances
+ws <- ws |> 
+  group_by(TierID) |> 
   mutate(sl = sqrt((E - lead(E))^2 + (N - lead(N))^2))
 
+# speed (m/s)
 ws <- ws |> 
   mutate(speed_ms = sl/timelag)
-ws
 
 ggplot(ws, aes(speed_ms)) +
   geom_histogram() +
