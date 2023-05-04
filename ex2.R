9da15474293ceefa350a54ebe59fe26fc5aa87e7
Thomas Rempfler <rempftho@students.zhaw.ch>
2023-05-04 08:27:43 +0200

    task 4 completed

--- ex2.R
+++ ex2.R
@@ -57,7 +57,11 @@ ws |>
 ggplot(ws, aes(DatetimeUTC, timelag/60, colour = TierName)) +
   geom_point()
 
-
+ws |> 
+  group_by(TierID) |> 
+  summarise(mean_timelag = mean(timelag, na.rm = T))
+    
+    
 # task 3 ----
 
 # euclidean distances
@@ -74,10 +78,90 @@ ggplot(ws, aes(speed_ms)) +
   scale_y_log10()
 
 
+# task 4 ----
+caro <- read_delim("caro60.csv", ",")
 
+# every 3rd position
+caro_3 <- caro |> 
+  slice(seq(from = 1, to = nrow(caro), by = 3))
 
-ws |> # Take wildschwein...
-  group_by(TierID) |> # ...group it by TierID
-  summarise( # Summarise the data...
-    mean_timelag = mean(timelag, na.rm = T) # ...by calculating the mean timelag
-  )
+# every 6th position
+caro_6 <- caro |> 
+  slice(seq(from = 1, to = nrow(caro), by = 6))
+
+# every 9th position
+caro_9 <- caro |> 
+  slice(seq(from = 1, to = nrow(caro), by = 9))
+
+# timelage, step length, speed for every caro_x
+caro <- st_as_sf(caro, coords = c("E", "N"), crs = 2056, remove = FALSE) |> 
+  mutate(timelag = as.numeric(difftime(lead(DatetimeUTC), DatetimeUTC), units = "mins")) |> 
+  mutate(sl = sqrt((E - lead(E))^2 + (N - lead(N))^2)) |> 
+  mutate(speed_ms = sl/timelag)
+
+caro_3 <- st_as_sf(caro_3, coords = c("E", "N"), crs = 2056, remove = FALSE) |> 
+  mutate(timelag = as.numeric(difftime(lead(DatetimeUTC), DatetimeUTC), units = "mins")) |> 
+  mutate(sl = sqrt((E - lead(E))^2 + (N - lead(N))^2)) |> 
+  mutate(speed_ms = sl/timelag)
+
+caro_6 <- st_as_sf(caro_6, coords = c("E", "N"), crs = 2056, remove = FALSE) |> 
+  mutate(timelag = as.numeric(difftime(lead(DatetimeUTC), DatetimeUTC), units = "mins")) |> 
+  mutate(sl = sqrt((E - lead(E))^2 + (N - lead(N))^2)) |> 
+  mutate(speed_ms = sl/timelag)
+
+caro_9 <- st_as_sf(caro_9, coords = c("E", "N"), crs = 2056, remove = FALSE) |> 
+  mutate(timelag = as.numeric(difftime(lead(DatetimeUTC), DatetimeUTC), units = "mins")) |> 
+  mutate(sl = sqrt((E - lead(E))^2 + (N - lead(N))^2)) |> 
+  mutate(speed_ms = sl/timelag)
+
+# visualisations
+
+caro_vis <- caro |> 
+  bind_rows(caro_3, caro_6, caro_9) |> 
+  filter(!is.na(timelag)) |> 
+  mutate(timelag = as.factor(timelag)) 
+
+# speed at different intervals (speed decreases with increasing timelag)
+ggplot(caro_vis, aes(DatetimeUTC, speed_ms, colour = timelag)) +
+  geom_line(linewidth = 1) + 
+  ggtitle("Comparing derived speed at different sampling intervals") +
+  theme_minimal() +
+  xlab("Time") +
+  ylab("Speed [m/s]")
+
+# comparison of trajectories
+caro_caro_3 <- caro |> 
+  bind_rows(caro_3) |> 
+  filter(!is.na(timelag)) |> 
+  mutate(timelag = as.factor(timelag))
+
+ggplot(caro_caro_3, aes(E, N, colour = timelag)) +
+  geom_point() + 
+  geom_path() +
+  coord_cartesian() +
+  ggtitle("Comparing original- with 3 minutes-resampled data") +
+  theme_minimal() 
+
+caro_caro_6 <- caro |> 
+  bind_rows(caro_6) |> 
+  filter(!is.na(timelag)) |> 
+  mutate(timelag = as.factor(timelag))
+
+ggplot(caro_caro_6, aes(E, N, colour = timelag)) +
+  geom_point() + 
+  geom_path() +
+  coord_cartesian() +
+  ggtitle("Comparing original- with 6 minutes-resampled data") +
+  theme_minimal() 
+
+caro_caro_9 <- caro |> 
+  bind_rows(caro_9) |> 
+  filter(!is.na(timelag)) |> 
+  mutate(timelag = as.factor(timelag))
+
+ggplot(caro_caro_9, aes(E, N, colour = timelag)) +
+  geom_point() + 
+  geom_path() +
+  coord_cartesian() +
+  ggtitle("Comparing original- with 9 minutes-resampled data") +
+  theme_minimal() 
